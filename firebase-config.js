e1ef2a556b3767d4a045a3d3379c4dfb4e54f036
dsr57894 <157563482+dsr57894@users.noreply.github.com>
2024-01-25 01:55:29 -0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC14V5cJYV8NawqmQTsZVczaDXDJzDzQCg",
+  authDomain: "bve-hyt6tfr.firebaseapp.com",
+  databaseURL: "https://bve-hyt6tfr-default-rtdb.firebaseio.com",
+  projectId: "bve-hyt6tfr",
+  storageBucket: "bve-hyt6tfr.appspot.com",
+  messagingSenderId: "636918464740",
+  appId: "1:636918464740:web:9dacfc35d4a61360b9acf1"
+};
